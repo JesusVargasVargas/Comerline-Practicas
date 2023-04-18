9d34a9ac0ebbd5013dfaf25a07b86ba8785e1c39
JesusVargasVargas <51136507+JesusVargasVargas@users.noreply.github.com>
2023-04-18 04:16:47 +0200

    Uso de RestTemplate

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,63 +4,37 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import static org.junit.Assert.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @GetMapping("/chuck")
+  public String chuck(@RequestParam(value = "quote", defaultValue = "I'm Chuck!") String quote) {
+
+    try {
       
-      SpringApplication.run(DemoApplication.class, args);
+      RestTemplate restTemplate = new RestTemplate();
+      String url = "https://api.chucknorris.io/jokes/random";
+      ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+      assertEquals(response.getStatusCode(), HttpStatus.OK);
+      quote = response.getBody();
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    @GetMapping("/chuck")
-    public String chuck(@RequestParam(value = "quote", defaultValue = "I'm Chuck!") String quote) {
 
-      try {
-
-        URL url = new URL("https://api.chucknorris.io/jokes/random");
-
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.connect();
-
-        int responsecode = conn.getResponseCode();
-
-        if (responsecode != 200) {
-            throw new RuntimeException("HttpResponseCode: " + responsecode);
-        } else {
-
-            String inline = "";
-            Scanner scanner = new Scanner(url.openStream());
-
-            while (scanner.hasNext()) {
-                inline += scanner.nextLine();
-            }
-
-            scanner.close();
-
-            JSONParser parse = new JSONParser();
-            JSONObject data_obj = (JSONObject) parse.parse(inline);
-
-            JSONObject obj = data_obj;
-
-            quote = (String) obj.get("value");
-            
-        }
-
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-
-      return quote;
-    }
+    return quote;
+    
+  }
 }
